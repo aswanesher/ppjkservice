c5d17470c1747baa1d40c61a5c064ef81c3a0984
aswanesher <aswansetiawan01@gmail.com>
2016-09-07 15:31:49 +0700

    no message

--- application/views/templates/frontend/ppjkservice/home.php
+++ application/views/templates/frontend/ppjkservice/home.php
@@ -8,10 +8,40 @@ if(!empty($slider_widget)) {
 
 <!-- WIDGET PAGE -->
 <?php 
-if(!empty($page_widget)) {
+/*if(!empty($page_widget)) {
     $this->load->view($temp.'widget/page_widget');
-}   
+} */  
 ?>
+<section class="about-info-box sec-padding">
+	<div class="thm-container">
+		<div class="row">
+			<div class="col-lg-4 hidden-md hidden-sm hidden-xs">
+				<div class="img-cap-effect">
+					<div class="img-box">
+						<img src="images/about-info-box/2.jpg" alt="Awesome Image"/>
+						<div class="img-caption">
+							<div class="box-holder"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-8 col-md-12">
+				<div class="sec-title">
+					<h2><span>Who We Are</span></h2>
+					<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+					<ul class="bulleted-list">
+						<li><i class="fa fa-arrow-circle-right"></i> Deliver Environmentally Responsible Client Services</li>
+						<li><i class="fa fa-arrow-circle-right"></i> Provide Employees with an Attractive Working Environment</li>
+						<li><i class="fa fa-arrow-circle-right"></i> Be an Active Community Partner</li>
+						<li><i class="fa fa-arrow-circle-right"></i> Maintain High Ethical Standards</li>
+						<li><i class="fa fa-arrow-circle-right"></i> Drive Continuous Improvement</li>
+					</ul>
+					<a href="#" class="thm-btn">view our services <i class="fa fa-arrow-right"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <!-- END WIDGET PAGE -->
 
 <!-- WIDGET BLOG -->
